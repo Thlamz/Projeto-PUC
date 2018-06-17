f3d4a1e59afde7fa78d8db88f6cee5e73bd422d7
Thlamz <thlamenza@hotmail.com>
2018-06-17 13:14:13 -0300

    função movimento
    move o asteroide

--- main.lua
+++ main.lua
@@ -1,22 +1,32 @@
+msgr = require('mqttLoveLibrary')
+
+
 function love.load()
+    minhamat = '1810981'
     
+    msgr.start('love',minhamat,coord_mov) -- Ao receber mensagem executa a função coord_mov
+    mov = '' --Estado inicial da movimentação
+    timer = 0
+    
+
     love.window.setTitle('Projeto Final')
-    
-    w,h = love.graphics.getDimensions() -- Dimensões(global)
     love.graphics.setBackgroundColor(0,0,0)
+    w,h = love.graphics.getDimensions() -- Dimensões(global)
+    
     
     asteroide = love.graphics.newImage('asteroide.png')
     
+    
     math.randomseed(os.time())
     
-    rot = 0
-    asx = w/2 -- X inicial do asteroide
-    k = 300
     
-    ntri = 5 -- Numero de trilhas do asteroide
+    rot = 0 -- Rotação original
+    asx = w/2 -- X inicial do asteroide
+    k = 300 -- Constante de movimentação
+    ntri = 5 -- Numero de trilhas de asteroide
     trilha = {}
     for i=1,ntri do 
-        trilha[i] = {x=w/2,y=h} -- 3 trilhas com valores default
+        trilha[i] = {x=w/2,y=h} -- Inicializa "ntri" trilhas com valores default
     end
     
 end
@@ -30,7 +40,7 @@ function desenha_asteroide()
     love.graphics.draw(asteroide,asx,3/4*h,rot,1,1,ax/2,ay/2)
     
     
-    love.graphics.circle('line',w/2,3/4*h,ay/2)
+    love.graphics.circle('line',asx,3/4*h,ay/2)
 end
 
 
@@ -66,11 +76,62 @@ function desenha_trilha()
     end
 end
 
+
+function coord_mov(msg) -- coordena a movimentação
+    print('mensagem recebida')
+    if msg=='esq' then
+        mov = 'esq'
+    end
+    
+    if msg=='dir' then
+        mov = 'dir'
+    end
+end
+
+function exec_mov(dt) -- Executa movimento
+    
+    if asx>w/2+(30*k*dt*2) and mov=='dir' then --Impede que saia da tela pela direita
+        mov = ''
+    end
+    
+    if asx<w/2-(30*k*dt*2) and mov=='esq' then --Impede que saia da tela pela esquerda
+        mov = ''
+    end
+    
+    if math.abs(asx - w/2)<30 and timer==0 then --Centraliza o asteroide para compensar pequenos erros
+        asx = w/2
+    end
+    
+    
+    
+    if mov=='esq' then
+        timer = timer + 1
+        
+        asx = asx - k*dt*2
+    
+    elseif mov=='dir' then
+        timer = timer + 1
+        
+        asx = asx + k*dt*2
+    
+    else
+        timer = 0
+    end
+    
+    
+    if timer >= 30 then
+        mov = ''
+    end
+    
+end
+
 function love.update(dt)
     
     rot = rot + dt*math.pi/5 -- Rotaciona o asteroide a cada update
     
+    msgr.checkMessages()
     cria_trilha(dt)
+    exec_mov(dt)
 end
 
 
---
+++ node_jogo.lua
@@ -0,0 +1,60 @@
+local msgr = require "mqttNodeMCULibrary"
+
+local led1 = 6
+local led2 = 3
+local sw1 = 2
+local sw2 = 1
+
+local minhamat = "1810981"
+
+
+local tdif = 500000
+
+
+function mesq()
+    msgr.sendMessage("esq",minhamat)  
+    
+    gpio.trig(sw1)
+    gpio.trig(sw2)
+    
+    gpio.write(led1,gpio.HIGH)
+    tmr.delay(tdif)  
+    gpio.write(led1,gpio.LOW)
+    
+    gpio.trig(sw1, "down", mesq)
+    gpio.trig(sw2, "down", mdir)
+
+end
+
+function mdir()
+    msgr.sendMessage("dir",minhamat)
+    
+    gpio.trig(sw1)
+    gpio.trig(sw2)
+
+    gpio.write(led2,gpio.HIGH)
+    tmr.delay(tdif)  
+    gpio.write(led2,gpio.LOW)
+    
+    gpio.trig(sw1, "down", mesq)
+    gpio.trig(sw2, "down", mdir)
+    
+end
+
+
+
+
+gpio.mode(sw1, gpio.INPUT, gpio.PULLUP)
+gpio.mode(sw2, gpio.INPUT, gpio.PULLUP)
+gpio.mode(led1, gpio.OUTPUT)
+gpio.mode(led2, gpio.OUTPUT)
+
+
+
+gpio.trig(sw1, "down", mesq)
+gpio.trig(sw2, "down", mdir)
+
+function mensagemrecebida()
+end
+
+msgr.start('node', minhamat ,mensagemrecebida) -- use unique id
