ba1d78b705c4ba4d6caacdeaefe9af3626ef68c5
Thlamz <thlamenza@hotmail.com>
2018-06-18 18:39:28 -0300

    organização do .load e mais opções para movimentação

--- main.lua
+++ main.lua
@@ -2,33 +2,37 @@ msgr = require('mqttLoveLibrary')
 
 
 function love.load()
-    minhamat = '1810981'
     
-    msgr.start('love',minhamat,coord_mov) -- Ao receber mensagem executa a função coord_mov
-    mov = '' --Estado inicial da movimentação
-    dist = 0 -- Variavel auxiliar "distância", para a movimentação
-    
-
+    --Tela
     love.window.setTitle('Projeto Final')
     love.graphics.setBackgroundColor(0,0,0)
     w,h = love.graphics.getDimensions() -- Dimensões(global)
-    
-    
+    -------------------------------//-------------------------------
+    --Movimentação
+    msgr.start('love',minhamat,coord_mov) -- Ao receber mensagem executa a função coord_mov
+    vmov=1 -- Velocidade da movimentação
+    nfaixas = 3 -- Número de faixas na movimentação
+    mov = '' --Estado inicial da movimentação
+    dist = 0 -- Variavel auxiliar "distância", para a movimentação
+    nfaixas = nfaixas+1 --Não alterar(ajusta para o número real de faixas)
+    -------------------------------//------------------------------
+    --Sprites
     asteroide = love.graphics.newImage('asteroide.png')
-    
-    
-    math.randomseed(os.time())
-    
-    
-    rot = 0 -- Rotação original
-    asx = w/2 -- X inicial do asteroide
-    k = 300 -- Constante de movimentação
+    -------------------------------//-------------------------------
+    --Trilha
     ntri = 5 -- Numero de trilhas de asteroide
     trilha = {}
     for i=1,ntri do 
         trilha[i] = {x=w/2,y=h} -- Inicializa "ntri" trilhas com valores default
     end
-    
+    -------------------------------//-------------------------------
+    --Defaults
+    minhamat = '1810981' -- Sua matricula
+    rot = 0 -- Rotação original
+    asx = w*(math.floor(nfaixas/2))/(nfaixas) -- X inicial do asteroide
+    k = 300 -- Constante de movimentação
+    math.randomseed(os.time())
+    -------------------------------//------------------------------
 end
 
 
@@ -90,41 +94,39 @@ end
 
 function exec_mov(dt) -- Executa movimento
     
-   if asx>5*w/6 and mov=='dir' then --Impede que saia da tela pela direita
+   if asx>(nfaixas-1)*w/nfaixas and mov=='dir' then --Impede que saia da tela pela direita
         mov = ''
     end
     
-    if asx<w/6 and mov=='esq' then --Impede que saia da tela pela esquerda
+    if asx<w/nfaixas and mov=='esq' then --Impede que saia da tela pela esquerda
         mov = ''
     end
     
-    if math.abs(asx - w/2)<50 and dist==0 then --Centraliza o asteroide para compensar pequenos erros
-        asx = w/2
+    if math.abs(asx - w*(math.floor(nfaixas/2))/(nfaixas))<50 and dist==0 then --Centraliza o asteroide para compensar pequenos erros
+        asx = w*(math.floor(nfaixas/2))/(nfaixas)
     end
     
     
     if mov=='esq' then
-        dist = dist + (2/6*w)*dt*3 -- Essa formulas garante velocidade fixa independente da velocidade do computador
+        dist = dist + (1/nfaixas*w)*dt*vmov -- Essa formulas garante velocidade fixa independente da velocidade do computador
         
-        asx = asx - (2/6*w)*dt*3
+        asx = asx - (1/nfaixas*w)*dt*vmov
     
     elseif mov=='dir' then
-        dist = dist + (2/6*w)*dt*3
-        
-        asx = asx + (2/6*w)*dt*3
+        dist = dist + (1/nfaixas*w)*dt*vmov
+        asx = asx + (1/nfaixas*w)*dt*vmov
     
     else
         dist = 0
     end
     
-    if dist >= 2/6*w then
+    if dist >= 1/nfaixas*w then
         mov = ''
     end
 end
 
 
 function love.update(dt)
-
     rot = rot + dt*math.pi/5 -- Rotaciona o asteroide a cada update
     
     msgr.checkMessages()
